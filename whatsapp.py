2a1a6ec31a7c2db20eec89d2c62fdebb72ea5f17
VISWESWARAN1998 <visweswaran.nagasivam98@gmail.com>
2017-09-02 20:05:50 +0530

    Add files via upload

---
+++ whatsapp.py
@@ -0,0 +1,186 @@
+﻿# SWAMI KARUPPASWAMI THUNNAI
+
+# ============================================================
+# Simple yet Hackable! WhatsApp API [UNOFFICIAL] for Python3
+# Note: The author gives permission to use it under Apache 2.0
+# Special Thanks To: alecxe, For reviewing my code!
+# ============================================================
+
+import time
+import datetime as dt
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+       
+
+class WhatsApp:
+    """
+    This class is used to interact with your whatsapp [UNOFFICIAL API]
+    """
+    emoji = {}  # This dict will contain all emojies needed for chatting
+    browser = webdriver.Chrome()  # we are using chrome as our webbrowser
+    timeout = 10  # The timeout is set for about ten seconds
+    
+    # This constructor will load all the emojies present in the json file and it will initialize the webdriver
+    def __init__(self, wait, screenshot=None):
+        self.browser.get("https://web.whatsapp.com/")
+        # emoji.json is a json file which contains all the emojis
+        with open("emoji.json") as emojies:
+            self.emoji = json.load(emojies)  # This will load the emojies present in the json file into the dict
+        WebDriverWait(self.browser,wait).until(EC.presence_of_element_located(
+            (By.CLASS_NAME, "input-search")))
+        if screenshot is not None:
+            self.browser.save_screenshot(screenshot)  # This will save the screenshot to the specified file location
+        
+    # This method is used to send the message to the individual person or a group
+    # will return true if the message has been sent, false else
+    def send_message(self, name, message):
+        message = self.emojify(message)  # this will emojify all the emoji which is present as the text in string
+        search = self.browser.find_element_by_class_name("input-search")
+        search.send_keys(name+Keys.ENTER)  # we will send the name to the input key box
+        current_time = time.time()
+        try:
+            send_msg = WebDriverWait(self.browser,self.timeout).until(EC.presence_of_element_located(
+                (By.CLASS_NAME, "input")))
+            send_msg.send_keys(message+Keys.ENTER)  # send the message
+            return True
+        except TimeoutException:
+            raise TimeoutError("Your request has been timed out! Try overriding timeout!")
+        except NoSuchElementException:
+            return False
+        except Exception:
+            return False
+                
+    # This method will count the no of participants for the group name provided
+    def participants_for_group(self, group_name):
+        search = self.browser.find_element_by_class_name("input-search")
+        search.send_keys(group_name+Keys.ENTER)  # we will send the name to the input key box
+        # some say this two try catch below can be grouped into one
+        # but I have some version specific issues with chrome [Other element would receive a click]
+        # in older versions. So I have handled it spereately since it clicks and throws the exception
+        # it is handled safely
+        try:
+            click_menu = WebDriverWait(self.browser,self.timeout).until(EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "header.pane-header:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1)")))
+            click_menu.click()
+        except TimeoutException:
+            raise TimeoutError("Your request has been timed out! Try overriding timeout!")
+        except NoSuchElementException as e:
+            return "None"
+        except Exception as e:
+            return "None"
+        current_time = dt.datetime.now()
+        participants_css_selector = "div.animate-enter2:nth-child(4) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2)"
+        while True:
+            try:
+                participants_count = self.browser.find_element_by_css_selector(participants_css_selector).text
+                if "256" in participants_count:
+                    return participants_count
+            except Exception as e:
+                pass
+            new_time = dt.datetime.now()
+            elapsed_time = (new_time - current_time).seconds
+            if elapsed_time > self.timeout:
+                return "NONE"             
+            
+    # This method is used to get the main page
+    def goto_main(self):
+        self.browser.get("https://web.whatsapp.com/")
+    
+    # get the status message of a person 
+    # TimeOut is approximately set to 10 seconds
+    def get_status(self, name):
+        search = self.browser.find_element_by_class_name("input-search")
+        search.send_keys(name+Keys.ENTER)  # we will send the name to the input key box
+        try:
+            group_xpath = "/html/body/div/div/div/div[3]/header/div[1]/div/span/img"
+            click_menu = WebDriverWait(self.browser,self.timeout).until(EC.presence_of_element_located(
+                (By.XPATH, group_xpath)))
+            click_menu.click()
+        except TimeoutException:
+            raise TimeoutError("Your request has been timed out! Try overriding timeout!")
+        except NoSuchElementException:
+            return "None"
+        except Exception:
+            return "None"
+        try:
+            status_css_selector = ".drawer-section-body > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)"   # This is the css selector for status
+            WebDriverWait(self.browser,self.timeout).until(EC.presence_of_element_located(
+                (By.CSS_SELECTOR, status_css_selector)))
+            status = self.browser.find_element_by_css_selector(status_css_selector).text
+            # We will try for 100 times to get the status
+            for i in range(10):
+                if len(status) > 0:
+                    return status
+                else:
+                    time.sleep(1) # we need some delay
+            return "None"
+        except TimeoutException:
+            raise TimeoutError("Your request has been timed out! Try overriding timeout!")
+        except NoSuchElementException:
+            return "None"
+        except Exception:
+            return "None"        
+    
+    # to get the last seen of the person
+    def get_last_seen(self, name, timeout=10):
+        search = self.browser.find_element_by_class_name("input-search")
+        search.send_keys(name+Keys.ENTER)  # we will send the name to the input key box
+        last_seen_css_selector = ".chat-subtitle-text"
+        start_time = dt.datetime.now()
+        try:
+            WebDriverWait(self.browser,self.timeout).until(EC.presence_of_element_located(
+                (By.CSS_SELECTOR, last_seen_css_selector)))
+            while True:
+                last_seen = self.browser.find_element_by_css_selector(last_seen_css_selector).text
+                if last_seen and "click here" not in last_seen:
+                    return last_seen
+                end_time = dt.datetime.now()
+                elapsed_time = (end_time-start_time).seconds
+                if elapsed_time > 10:
+                    return "None"
+        except TimeoutException:
+            raise TimeoutError("Your request has been timed out! Try overriding timeout!")
+        except NoSuchElementException:
+            return "None"
+        except Exception:
+            return "None"
+     
+    # This method does not care about anything, it sends message to the currently active chat
+    # you can use this method to recursively send the messages to the same person
+    def send_blind_message(self, message):
+        try:
+            message = self.emojify(message)
+            send_msg = self.browser.find_element_by_class_name("input")
+            send_msg.send_keys(message+Keys.ENTER)  # send the message
+            return True
+        except selenium.common.exceptions.NoSuchElementException:
+            return "Unable to Locate the element"
+        except Exception as e:
+            return False    
+     
+    # override the timeout
+    def override_timeout(self, new_timeout):
+        self.timeout = new_timeout
+    
+    # This method is used to emojify all the text emoji's present in the message
+    def emojify(self,message):
+        for emoji in self.emoji:
+            message = message.replace(emoji,self.emoji[emoji])
+        return message
+    
+    # This method is used to quit the browser
+    def quit(self):
+        self.browser.quit()
+    
+        
+        
+    
+        
+
+
