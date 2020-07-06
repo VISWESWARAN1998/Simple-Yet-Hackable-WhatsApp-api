beee8b467a30f912f43eb88c0a549f0bc41e3de2
aahnik <aahnikdaw@gmail.com>
2020-07-06 15:15:51 +0530

    updated conf

--- conf.py
+++ conf.py
@@ -1,2 +1,2 @@
-CHROME_DRIVER_PATH = ''
+CHROME_DRIVER_PATH = '/home/aahnik/Downloads/Applications/chromedriver_linux64/chromedriver'
 # store the path of chrome driver installation in this variable
