5ce149f20d4b34f092b8e0f3fda8410a97bb84e6
aahnik <aahnikdaw@gmail.com>
2020-07-06 15:18:14 +0530

    some up

---
+++ send.py
@@ -0,0 +1,9 @@
+msg = '''	laughing  :-d 
+	neutral_face :-|, 
+	stuck_out_tongue  :-p
+	heart  <3 
+	simple_smile :-) 
+	":worried:" : ":-(", 
+	":-1:" : "(n)", 
+	":thumbsup:" : "(y)", 
+	":wink:" : ";-)"'''
